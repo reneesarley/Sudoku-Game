a04b32bec208f823484a93c41a8db0a6732be5a1
Renee Sarley <reneesarley@gmail.com>
2018-11-18 19:57:41 -0800

    update solution service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { GameboardComponent } from './gameboard/gameboard.component';
 import { NumberBoxComponent } from './number-box/number-box.component';
@@ -10,10 +11,12 @@ import { NumberBoxComponent } from './number-box/number-box.component';
     AppComponent,
     GameboardComponent,
     NumberBoxComponent,
+
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/solution.service.ts
+++ src/app/solution.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { SOLUTIONS } from './solutions';
-import { Solution } from './solution';
+import Solution from './solution';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SolutionService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getSolutions(): Observable<Solution[]>{
     return of(SOLUTIONS);
